74a58900f3e1fb714401d1bd315b84a4a88d4152
zacharynichol <zachary.nichol@gmail.com>
2014-11-22 11:03:34 -0500

    Testing commit to online hub
    just testing a comment update.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## updating the comments to test if I can commit a change to GitHub
 
 ## Write a short comment describing this function
 
